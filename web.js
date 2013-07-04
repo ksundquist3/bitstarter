717bd7c202d1bc1e146550625a600e0e7049fb53
Kristen Sundquist <kristen@csc8.com>
2013-07-04 10:00:44 -0700

    Modified web.js to turn text into a string without using a buffer

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require('express');
 var fs = require('fs');
 
 var text = fs.readFileSync('index.html');
-var buf = new Buffer(text, "utf-8");
+// var buf = new Buffer(text, "utf-8");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString("utf-8"));
+  response.send(text.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
